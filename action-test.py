9d263eca3b6ea48e8c7298a3924528841938321e
binarytested <45023617+binarytested@users.noreply.github.com>
2018-11-20 13:43:36 -0500

    Update action-test.py

--- action-test.py
+++ action-test.py
@@ -4,19 +4,18 @@ from hermes_python.hermes import Hermes
 #globalvalue = "global"
 
 def test_code(hermes, intent_message):
-    
     #globalvalue = "eureka"
     #INTENT_FILTER = ["test_end_session"]
-    sentence = "continue session"
+    #sentence = "continue session"
     #hermes.publish_continue_session(intent_message.session_id, sentence, INTENT_FILTER)
-    hermes.publish_end_session(intent_message.session_id, sentence)
-    
     #print('Intent {}'.format(intent_message.intent))
 
     #for (slot_value, slot) in intent_message.slots.items():
     #    print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
 
     #hermes.publish_end_session(intent_message.session_id, 'Ending session')
+    print("*** EUREKA ***")
+    hermes.publish_end_session(intent_message.session_id, sentence)
      
    
 #def test_end_session(hermes, intent_message):   
