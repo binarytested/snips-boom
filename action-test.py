9e5c153bdb260bf064a3f062d700ef002bfd5808
binarytested <45023617+binarytested@users.noreply.github.com>
2018-11-25 20:10:40 -0500

    Update action-test.py

--- action-test.py
+++ action-test.py
@@ -1,36 +1,109 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+from pyfmadmin import pyfmadmin
 
-globalvalue = "global"
+CONFIG_INI = "config.ini"
 
-def test_code(hermes, intent_message):
-    globalvalue = "eureka"
-    INTENT_FILTER = ["multip:test_end_session"]
-    coming_intent = intent_message.intent.intent_name
-    
-    if coming_intent == "multip:test_code":
-        sentence = "start session"
-        hermes.publish_continue_session(intent_message.session_id, sentence, INTENT_FILTER)
-    if coming_intent == "multip:test_end_session":
-        sentence = "end session"
-        hermes.publish_end_session(intent_message.session_id, sentence)
-    #sentence = "continue session"
-    #hermes.publish_continue_session(intent_message.session_id, sentence, INTENT_FILTER)
-    #print('Intent {}'.format(intent_message.intent))
 
-    #for (slot_value, slot) in intent_message.slots.items():
-    #    print('Slot {} -> \n\tRaw: {} \tValue: {}'.format(slot_value, slot[0].raw_value, slot[0].slot_value.value.value))
+INTENT_DISCONNECT = "multip:disconnect_from_server"
 
-    #hermes.publish_end_session(intent_message.session_id, 'Ending session')
-    #print("*** EUREKA ***")
-    #hermes.publish_end_session(intent_message.session_id, sentence)
-     
-   
-def test_end_session(hermes, intent_message):   
-    hermes.publish_end_session(intent_message.session_id, globalvalue)
+INTENT_FILTER = [INTENT_DISCONNECT]
 
-    
-    
-with Hermes('raspberrypi.local:1883') as h:
-    h.subscribe_intents(test_code).start()
-    #h.subscribe_intent("multip:test_code", test_code).start()
+# If this skill is supposed to run on the satellite,
+# please get this mqtt connection info from <config.ini>
+# Hint: MQTT server is always running on the master device
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
+
+class snips_fmadmin(object):
+    """Class used to wrap action code with mqtt connection
+        
+        Please change the name refering to your application
+    """
+
+    def __init__(self):
+        # get the configuration if needed
+        try:
+            self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+        except :
+            self.config = None
+                
+        self.fa = pyfmadmin (str(self.config["secret"]["hostname"]), str(self.config["secret"]["username"]), str(self.config["secret"]["password"]))
+
+        # start listening to MQTT
+        self.start_blocking()
+        
+        
+    def getIntentName (self, intent_message):
+        intentName = intent_message.intent.intent_name
+        intentName = intentName.split(":")[1]
+        return intentName
+        
+        
+    # --> Sub callback function, one per intent
+    def connect_to_server(self, hermes, intent_message):
+
+        hermes.publish_start_session_notification(intent_message.site_id, "Attempting connection to server...", "")
+        
+        loginResponse = self.fa.login()
+        
+        if loginResponse["result"] == 0:
+	        print ( "Login successful!" )
+	        hermes.publish_continue_session(intent_message.session_id, "Connected to server. What would you like to do?", INTENT_FILTER)
+	        #hermes.publish_end_session(intent_message.session_id, "Connected to server")
+        else:
+	        print ( "Login failed" )
+	        hermes.publish_end_session(intent_message.session_id, "Unable to connect to server")
+
+
+        # if need to speak the execution result by tts
+        #hermes.publish_start_session_notification(intent_message.site_id, "Action1 has been done", "")
+
+
+
+
+    def disconnect_from_server(self, hermes, intent_message):
+        # terminate the session first if not continue
+        hermes.publish_end_session(intent_message.session_id, "Disconnecting from server")
+        
+        logoutResponse = self.fa.logout()
+        
+        if logoutResponse["result"] == 0:
+	        print ( "Logout successful!" )
+        else:
+	        print ( "Logout failed" )
+
+        # if need to speak the execution result by tts
+        #hermes.publish_start_session_notification(intent_message.site_id, "Action2 has been done", "")
+
+    # More callback function goes here...
+
+
+
+
+
+    # --> Master callback function, triggered everytime an intent is recognized
+    def master_intent_callback(self,hermes, intent_message):
+        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+        #coming_intent = intent_message.intent.intent_name
+        coming_intent = self.getIntentName(intent_message)
+        if coming_intent == 'connect_to_server':
+            self.connect_to_server(hermes, intent_message)
+        if coming_intent == 'disconnect_from_server':
+            self.disconnect_from_server(hermes, intent_message)
+
+        # more callback and if condition goes here...
+
+    # --> Register callback function and start MQTT
+    def start_blocking(self):
+        with Hermes(MQTT_ADDR) as h:
+            h.subscribe_intents(self.master_intent_callback).start()
+
+if __name__ == "__main__":
+    snips_fmadmin()
